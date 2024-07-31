bac181befcd67f2a5eb5affacf43ed08ca22786b
Mohammed Irshath <110977743+MohammedIrshad123@users.noreply.github.com>
2024-07-31 22:47:51 +0530

    Update Series.java
    Simple changes

--- example/src/example/Series.java
+++ example/src/example/Series.java
@@ -1,18 +1,15 @@
-
-/*We use the integers a,b, and n  to create the following series:
- 
+/*We use the integers a,b, and n  to create the following series: 
  (a+2^0*b),(a+2^0*b+2^1.b),..,(a+2^0*b+2^1.b+...+2^n-1.b)
-
 You are given  queries in the form of a, b and n. For each query, 
 print the series corresponding to the given a, b, and n  values as a single line of  space-separated integers.
 */
-package example;
 
+package example;
 import java.util.*;
 public class Series {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		        Scanner in = new Scanner(System.in);
 		        int t = in.nextInt();
 		        
@@ -26,13 +23,10 @@ public class Series {
 		                z += (int)Math.pow(2, j) * b;
 		                System.out.print(z + " ");
 		            }
-		            System.out.println();
-		        }
+		            		        }
 		        
 		        in.close();
 		    }
-		
-
 	
 
 }
